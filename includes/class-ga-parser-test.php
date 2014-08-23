79dc9aa64a37b1ff607970f2fa06fa36480bd742
Sudar <sudar@sudarmuthu.com>
2014-08-23 12:34:10 +0530

    Add test file

---
+++ includes/class-ga-parser-test.php
@@ -0,0 +1,19 @@
+<?php
+require_once( 'class-ga-parser.php' );
+
+$ga_parser = new GA_Parser();
+
+if ( $ga_parser->cookie_parsed() ) {
+    $campaign_info            = array();
+
+    $campaign_info['source']  = trim( $ga_parser->campaign_source );
+    $campaign_info['name']    = trim( $ga_parser->campaign_name );
+    $campaign_info['medium']  = trim( $ga_parser->campaign_medium );
+    $campaign_info['term']    = trim( $ga_parser->campaign_term );
+    $campaign_info['content'] = trim( $ga_parser->campaign_content );
+
+    print_r( $campaign_info );
+} else {
+    echo "Cookie not found";
+}
+?>
